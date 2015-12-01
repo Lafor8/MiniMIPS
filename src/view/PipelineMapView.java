db507a2ed0cab822aa9e8b48db46c7fc675f366a
Lafor8 <jmtw_84@yahoo.com>
2015-12-01 08:38:21 +0800

    minor changes

--- src/view/PipelineMapView.java
+++ src/view/PipelineMapView.java
@@ -5,15 +5,24 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
+import net.miginfocom.swing.MigLayout;
 
 public class PipelineMapView extends JPanel {
 	
 	
 	public JTable pipelineMapTable;
 	TitledBorder title = BorderFactory.createTitledBorder("Pipeline Map");
+	private JTable table;
 	
 	public PipelineMapView(){
 		setBorder(title);
+		setLayout(new MigLayout("", "[grow]", "[grow]"));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, "cell 0 0,grow");
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 	}
 	
 	public void refresh(){
